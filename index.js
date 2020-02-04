f8f640cb74f21a2bd02854fb9bb0ab410b580382
Danny Wong <wong.danny29@gmail.com>
2020-02-03 16:46:03 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = "Njc0MDQ1Nzk1NTM5ODc3OTQx.Xji5_w.IB11HKh89DY085EHxiCOKvHoy5s";//process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+        msg.channel.send('pong');
+
+    } else if (msg.content.startsWith('!kick')) {
+        if (msg.mentions.users.size) {
+            const taggedUser = msg.mentions.users.first();
+            msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+        } else {
+            msg.reply('Please tag a valid user!');
+        }
+    }
+});
